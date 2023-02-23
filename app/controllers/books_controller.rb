444cf775cc987e028de3c1ee4588fc3dc62b3eb0
Strawhub <k7to.so23.ten717@i.softbank.jp>
2023-02-24 00:58:44 +0900

    Create new page to add book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,14 @@ class BooksController < ApplicationController
     def index
         @books = Book.all.order(created_at: 'desc')
     end
+
+    def show
+        @book = Book.find(params[:id])
+    end
+
+    def new
+    end
+
+    def created
+    end
 end
