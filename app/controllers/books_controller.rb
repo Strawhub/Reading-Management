96b1999e2796b92db28f177deef35a83399f5eea
Strawhub <k7to.so23.ten717@i.softbank.jp>
2023-02-27 21:53:35 +0900

    Add: Ability to delete items

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,6 +36,12 @@ class BooksController < ApplicationController
         end
     end
 
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to books_path
+    end
+
     private
         def book_params
             params.require(:book).permit(:title, :description)
