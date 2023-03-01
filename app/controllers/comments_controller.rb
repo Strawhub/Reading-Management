6903568e0b84e3a07a0ea2ee74b1ed0bf8c0e6b8
Strawhub <k7to.so23.ten717@i.softbank.jp>
2023-03-01 21:53:18 +0900

    Update: Delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
         redirect_to book_path(@book)
     end
 
+    def destroy
+        @book = Book.find(params[:book_id])
+        @comment = @book.comments.find(params[:id])
+        @comment.destroy
+        redirect_to book_path(@book)
+    end
+
     private
         def comment_params
             params.require(:comment).permit(:description)
