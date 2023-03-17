e1ed18540b8dfba0fdf27fb750c7b910a81df0a2
Strawhub <k7to.so23.ten717@i.softbank.jp>
2023-03-17 20:24:37 +0900

    deploy test2

--- Gemfile
+++ Gemfile
@@ -71,7 +71,3 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
-
-group :production do
-  gem 'pg' 
-end
--- render-build.sh
+++ render-build.sh
@@ -3,6 +3,7 @@
 # exit on error
 set -o errexit
 yarn install
+yarn build
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
