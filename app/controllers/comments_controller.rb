e5da3887c4b8be098d12e162b00ff0d24165b17b
Strawhub <k7to.so23.ten717@i.softbank.jp>
2023-03-21 00:50:14 +0900

    add: どのページに対してコメントしたのか入力・表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,6 @@ class CommentsController < ApplicationController
 
     private
         def comment_params
-            params.require(:comment).permit(:description)
+            params.require(:comment).permit(:description, :pages)
         end
 end
